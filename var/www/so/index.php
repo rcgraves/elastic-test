d7fef8e505e94df73110b88ae06d5f4de96a129e
doug <doug.burks@gmail.com>
2017-05-30 11:50:20 -0400

    update /var/www/so/index.php

---
+++ var/www/so/index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title>Security Onion: HOME</title>
+</head>
+<body style="background-color:#A3C3E0">
+<center> 
+<br> 
+<b> 
+<a href="https://www.securityonion.net"><img STYLE="border: none;" src="securityonion_logo.jpg" alt="Security Onion"></img></a><br>
+<a href="https://www.securityonion.net">https://www.securityonion.net</a><br> 
+<a href="https://www.securityonionsolutions.com">https://www.securityonionsolutions.com</a><br> 
+</b> 
+</center>
+<br> 
+<b>What is Security Onion?</b><br>
+Security Onion is a Linux distro for intrusion detection, network security monitoring, and log management. It's based on Ubuntu and contains Snort, Suricata, Bro, Elastic, OSSEC, Sguil, Squert, NetworkMiner, and many other security tools. The easy-to-use Setup wizard allows you to build an army of distributed sensors for your enterprise in minutes!<br>
+<br>
+<b>How do I install and configure Security Onion?</b><br>
+Please follow the <a href="https://github.com/Security-Onion-Solutions/security-onion/wiki/Installation">Installation guides</a> on our <a href="https://github.com/Security-Onion-Solutions/security-onion/wiki">Wiki</a>.<br>
+<br>
+<b>How do I access logs from this Security Onion installation?</b><br>
+Links to quickly access your local Squert and Elastic instances:<br>
+* <a href="/squert/">Squert</a>: View and categorize NIDS/HIDS alerts<br>
+* <a href="/app/kibana/">Elastic</a>: Search logs (IDS, Bro, and syslog)<br>
+<br>
+<b>How do I learn more about Security Onion?</b><br>
+Links to useful Security Onion information:<br>
+* <a href="http://blog.securityonion.net">Blog</a>: Get the latest news and updates</a><br>
+* <a href="https://github.com/Security-Onion-Solutions/security-onion/wiki">Wiki</a>: Table of Contents<br>
+* <a href="https://github.com/Security-Onion-Solutions/security-onion/wiki/Installation">Installation</a>: Installation guides<br>
+* <a href="https://github.com/Security-Onion-Solutions/security-onion/wiki/Tools">Tools</a>: List of included security tools<br>
+* <a href="https://github.com/Security-Onion-Solutions/security-onion/wiki/MailingLists">Mailing Lists</a>: Join the list(s) to get help and help others<br>
+* <a href="https://github.com/Security-Onion-Solutions/security-onion/wiki/Roadmap">Roadmap</a>: Preview future updates</a><br>
+<br> 
+<b>Commercial Support and Training</b><br>
+Need training or commercial support?<br>
+<a href="https://www.securityonionsolutions.com">https://www.securityonionsolutions.com</a><br> 
+<br> 
+<b>Disclaimer of Warranty<br>
+<small>THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM .AS IS. WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.</small><br>
+<br>
+<b>Limitation of Liability</b><br>
+<small>IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.</small><br>
+</body>
+</html>
+
